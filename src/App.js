397d912623ca96c0521ef3aee8e90199b11dacab
thevowels <aungphyohtet1@gmail.com>
2024-07-26 22:51:19 +0630

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,33 +8,50 @@ import {
   Code,
   Grid,
   theme,
+  Center,
+  Flex,
+  Image,
+  Badge,
+  Button,
 } from '@chakra-ui/react';
+import {MdStar} from 'react-icons/all';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <div id={"whole"}>
+        <div id="spacing" style={{height:'30px'}}>
+
+        </div>
+        <div id="heading">
+          <Box p="5"  borderWidth="1px"
+               borderRadius="15px"
+               backgroundColor="#f5f7ff"
+               marginLeft="10px"
+               marginRight="10px"
+          >
+              <Image src={require("./profile.png")}
+                     alt={"Aung Phyo Htet"}
+                     boxSize='300px'
+                     objectFit='cover'
+                     borderRadius='full'
+                     marginLeft='10%'
+
+
+              />
+            <Center maxW={"500px"}>
+
+            </Center>
+          </Box>
+        </div>
+      <div id={"body"} style={{ height:'50vh'}}>
+      </div>
+      <div id={"footer"} style={{height: '70vh'}}>
+
+      </div>
+      </div>
     </ChakraProvider>
   );
 }
