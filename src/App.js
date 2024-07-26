0c766739e2b772cd3dbfa0d8c8f460c19f67d8ce
thevowels <aungphyohtet1@gmail.com>
2024-07-27 01:27:01 +0630

    Responsive

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ import {
   Stack,
   Text,
   Button, Heading,
+  Flex, useBreakpointValue
 } from '@chakra-ui/react';
 import Abcd from './Abcd';
+import ResponsiveLayout from './ResponsiveLayout';
 
 function App() {
 
   const [count, setCount] = React.useState(0);
   const [show, setShow] = React.useState(false);
-  const clickHandler= (e)=>{
+  const clickHandler= ()=>{
+    console.log('clicked')
     setCount(count+1);
     if(count > 5){
       setShow(true);
@@ -26,44 +29,7 @@ function App() {
     <ChakraProvider theme={theme}>
       <div id={"whole"}>
         <div id="heading">
-          <Box p="5"  borderWidth="1px"
-               borderRadius="15px"
-               backgroundColor="#f5f7ff"
-               m="10px"
-               display="flex"
-               justifyContent="center"
-               alignItems="center"
-
-          >
-
-            <Image src={require("./profile.png")}
-                   alt={"Aung Phyo Htet"}
-                   boxSize='300px'
-                   objectFit='cover'
-                   borderRadius='full'
-                   display="flex"
-                   marginRight="100px"
-                   onClick={clickHandler}
-
-
-            />
-            <Box   display="flex"
-                   justifyContent="center"
-                   alignItems="center"
-            >
-              <Box alignItems={"center"}
-                   textAlign={"center"}
-              >
-                <Heading as="h1" size="lg">Aung Phyo Htet</Heading>
-                <br/>
-                <Heading as="h3" size={"md"} >Yangon, Myanmar</Heading>
-                <br/>
-                <Text as='u'>Web Developer</Text>
-
-              </Box>
-            </Box>
-
-          </Box>
+          <ResponsiveLayout btnClickHandler={clickHandler}/>
         </div>
         <div id={"body"} style={{ height:'50vh'}}>
         {show ?
@@ -74,8 +40,8 @@ function App() {
             <Image src={require('./puuung.gif')} />
           </Center>
           : <></>
-
         }
+
       </div>
       <div id={"footer"} style={{height: '70vh'}}>
 
---
+++ src/ResponsiveLayout.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Box, Flex, Heading, Image, Stack, Text, useBreakpointValue } from '@chakra-ui/react';
+
+function ResponsiveLayout({ btnClickHandler }) {
+  // Determine the layout based on screen size
+  const isMobile = useBreakpointValue({ base: true, md: false });
+  console.log(btnClickHandler);
+  return (
+    <Box p={5}>
+      {isMobile ? (
+        // Stack layout for mobile screens
+        <Stack spacing={4}
+               borderRadius="15px"
+               backgroundColor="#f5f7ff"
+               m="10px"
+               display="flex"
+               justifyContent="center"
+               alignItems="center"
+        >
+          <Image src={require("./profile.png")}
+                 alt={"Aung Phyo Htet"}
+                 boxSize='300px'
+                 objectFit='cover'
+                 borderRadius='full'
+                 display="flex"
+                 onClick={()=>btnClickHandler()}
+
+
+
+          />
+          <Box   display="flex"
+                 justifyContent="center"
+                 alignItems="center"
+          >
+            <Box alignItems={"center"}
+                 textAlign={"center"}
+            >
+              <Heading as="h1" size="lg">Aung Phyo Htet</Heading>
+              <br/>
+              <Heading as="h3" size={"md"} >Yangon, Myanmar</Heading>
+              <br/>
+              <Text as='u'>Web Developer</Text>
+
+            </Box>
+          </Box>        </Stack>
+      ) : (
+        // Flex layout for larger screens
+        <Flex direction="row" gap={30}
+              borderRadius="15px"
+              backgroundColor="#f5f7ff"
+              m="10px"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+        >
+          <Image src={require("./profile.png")}
+                 alt={"Aung Phyo Htet"}
+                 boxSize='300px'
+                 objectFit='cover'
+                 borderRadius='full'
+                 display="flex"
+
+
+          />
+          <Box   display="flex"
+                 justifyContent="center"
+                 alignItems="center"
+          >
+            <Box alignItems={"center"}
+                 textAlign={"center"}
+            >
+              <Heading as="h1" size="lg">Aung Phyo Htet</Heading>
+              <br/>
+              <Heading as="h3" size={"md"} >Yangon, Myanmar</Heading>
+              <br/>
+              <Text as='u'>Web Developer</Text>
+
+            </Box>
+          </Box>        </Flex>
+      )}
+    </Box>
+  );
+}
+
+export default ResponsiveLayout;
